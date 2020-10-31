525dabd5c1e7b17b800ad5b398b64adc46370f65
anshui <aggarwalanshul01@gmail.com>
2020-10-31 10:30:36 +0530

    db

--- db/loginTable.js
+++ db/loginTable.js
@@ -1,8 +1,9 @@
 const sequelize = require('sequelize');
-const db = new sequelize({
-    dialect: 'sqlite',
-    storage: __dirname + '/test.db'
-})
+const db = new sequelize('postgres://cvptviaaptsgte:f69ece92bce6597973abf21e42c05bd6c41b647ec99bf64a98387a8ac5ee717c@ec2-23-20-70-32.compute-1.amazonaws.com:5432/d941dkj96ohlhb')
+    // const db = new sequelize({
+    //     dialect: 'sqlite',
+    //     storage: __dirname + '/test.db'
+    // })
 const users = db.define('user', {
     id: {
         type: sequelize.DataTypes.INTEGER,
