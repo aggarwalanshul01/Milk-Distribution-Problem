0900ddbdf171281bbd11f148d79354e3f0435e40
anshui <aggarwalanshul01@gmail.com>
2020-10-31 09:33:27 +0530

    front

--- public/js/scriptLogged.js
+++ public/js/scriptLogged.js
@@ -5,7 +5,7 @@ $(() => {
         console.log(data);
         console.log(data.username);
         navbar.append(`
-        <form class="form-inline my-2 my-lg-0" method="GET" action="/profile" style="margin-left: 445px ">
+        <form class="form-inline my-2 my-lg-0" method="GET" action="/profile" style="margin-right: 0px; margin-right: 390px">
             <button type="submit" class="btn btn-outline-warning form-inline my-2 my-lg-0">${data.username}</button>
         `)
         addSingleManData();
